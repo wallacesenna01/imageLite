9412f6caa716273d9f503b7451e575af9f1bfb74
Wallace Artur <wallacearthur_3@hotmail.com>
2025-03-12 15:01:08 -0300

    Template Created

---
+++ src/app/formulario/page.tsx
@@ -0,0 +1,7 @@
+export default function FormularioPage() {
+    return (
+        <h1>
+            Formulario 
+        </h1>
+    )
+}
---
+++ src/app/galeria/page.tsx
@@ -0,0 +1,11 @@
+import { Template } from '../../components/Template'
+
+
+
+export default function GaleriaPage() {
+    return(
+        <Template>
+            <h1>Galeria</h1>
+        </Template>
+    )
+}
