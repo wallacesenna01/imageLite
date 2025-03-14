999e6f54135f1215cd3655cdcbd43c0db331557b
Wallace Artur <wallacearthur_3@hotmail.com>
2025-03-14 17:52:34 -0300

    fecth no back end concluido with sucess

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -9,17 +9,19 @@ import { useState } from 'react'
 export default function GaleriaPage() {
 
     const userService = useImageService();
-    const [images, setImages] = useState<Image[]>([]);
+    const [images, setImages] = useState<Image[]>([])
+    const [query, setQuery] = useState<string>('')
+    const [extension, setExtension] = useState<string>('')
 
      async function searchImages() {
-        const result = await userService.buscar();
+        console.log("valor digitado :" + query )
+        const result = await userService.buscar(query, extension);
         setImages(result);
-        console.table(result);
     }
 
     function cardImageRender(image:Image) {
         return (
-            <ImageCard nome={image.name} src={image.url} tamanho={image.size} dataUpload={image.uploadDate}/>
+            <ImageCard key={image.url} nome={image.name} src={image.url} tamanho={image.size} dataUpload={image.uploadDate}/>
         )
     }
 
@@ -32,10 +34,15 @@ export default function GaleriaPage() {
 
             <section className="flex flex-col items-center justify-center my-5">
                 <div className="flex space-x-4">
-                    <input type="text" className="border px-5 py-2 rounded-lg text-grey-900"/>
-                    <select className='border px-4 py-2 rounded-lg text-grey-900'>
-                        <option>All formats</option>
-                    </select>
+                    <input type="text"
+                          onChange={event => setQuery(event.target.value)}
+                          className="border px-5 py-2 rounded-lg text-grey-900"/>
+                         <select aria-label='selecione uma opção' onChange={event =>setExtension(event.target.value)} className='border px-4 py-2 rounded-lg text-grey-900'>
+                         <option value="">All formats</option>
+                         <option value="PNG">PNG</option>
+                         <option value="JPEG">JPEG</option>
+                         <option value="GIF">GIF</option>
+                          </select>
                     <button className='bg-blue-500 text-white px-4 py-2 rounded-lg' onClick={searchImages}>Search</button>
                     <button className='bg-yellow-500 text-white px-4 py-2 rounded-lg'>Add New</button>
 
