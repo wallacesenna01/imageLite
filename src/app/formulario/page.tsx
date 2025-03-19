50916f26d6893a793ddfe41e010d225a24e15085
Wallace Artur <wallacearthur_3@hotmail.com>
2025-03-19 10:07:59 -0300

    implements bindins inputs

--- src/app/formulario/page.tsx
+++ src/app/formulario/page.tsx
@@ -19,6 +19,8 @@ const formScheme: FormProps = {name: '', tags: '', file: ''}
 
 export default function FormularioPage() {
 
+
+    const [loading, setLoading] = useState<boolean>();
     const [imagePreview, setImagePreview] = useState<string>();
     const service = useImageService();
 
@@ -28,13 +30,15 @@ export default function FormularioPage() {
     })
 
      async  function handleSubmit (dados: FormProps) {
+        setLoading(true);
         const formData = new FormData();
         formData.append("file", dados.file)
         formData.append("name", dados.name)
         formData.append("tags", dados.tags)
-        await  service.salvar(formData)
+        await  service.salvar(formData);
         formik.resetForm();
-        setImagePreview('')
+        setImagePreview('');
+        setLoading(false);
     }
 
     function onFileUpload(event: React.ChangeEvent<HTMLInputElement>) {
@@ -47,18 +51,22 @@ export default function FormularioPage() {
     }
 
     return (
-        <Template>
+        <Template loading={loading}>
             <section className='flex flex-col items-center justify-center my-5'>
                 <h5 className='mt-3 mb-10 text-3xl font-extrabold tracking-tight text-slate-900'>Nova Imagem</h5>
                 <form onSubmit={formik.handleSubmit}>
                     <div className='grid grid-cols-1'>
                         <label className='block text-sm font-medium leading-6 text-gray-600'>Name: *</label>
-                        <InputText id='name' onChange={formik.handleChange} placeholder='type the name of the image'/>
+                        <InputText id='name' onChange={formik.handleChange}
+                        value={formik.values.name}
+                         placeholder='type the name of the image'/>
                     </div>
 
                     <div className=' mt-5 grid grid-cols-1'>
                         <label className='block text-sm font-medium leading-6 text-gray-600'>Tags: *</label>
-                        <InputText id='tags' onChange={formik.handleChange} placeholder='type the name of the tag'/>
+                        <InputText id='tags' onChange={formik.handleChange}
+                         value={formik.values.tags}
+                         placeholder='type the name of the tag'/>
                     </div>
 
                     <div className='mt-5 grid grid-cols-1'>
